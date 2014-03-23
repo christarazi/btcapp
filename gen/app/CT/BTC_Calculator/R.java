6511c5a9dfdf12052194818d87e8d9a873ebd0c6
christarazi <Chris Tarazi>
2014-03-23 01:30:19 -0700

    Updated name of app

--- gen/app/CT/BTC_Calculator/R.java
+++ gen/app/CT/BTC_Calculator/R.java
@@ -1,6 +1,6 @@
 /*___Generated_by_IDEA___*/
 
-package app.CT.BTC_Break_Even_Calculator;
+package app.CT.BTC_Calculator;
 
 /* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
 public final class R {
--- src/app/CT/BTC_Calculator/MyActivity.java
+++ src/app/CT/BTC_Calculator/MyActivity.java
@@ -1,4 +1,4 @@
-package app.CT.BTC_Break_Even_Calculator;
+package app.CT.BTC_Calculator;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -14,6 +14,7 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import app.CT.BTC_Calculator.R;
 
 import java.text.DecimalFormat;
 
